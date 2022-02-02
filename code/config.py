d7c0805e08ff7f183ee785756572939566d7e46e
pabloSanchezBB <t.rowland711@gmail.com>
2022-02-02 08:54:53 -0600

    made the workout dictionary

---
+++ code/config.py
@@ -0,0 +1,77 @@
+# This will be the config.py file containing workouts, date started
+
+starting_date = '01-25-22'
+
+workouts = { 'Push Day 1' : 
+				{ 
+					'Flat Bench' : '3s x 8r',
+					'Incline Bench' : '3s x 8r',
+					'Decline Bench' : '3s x 8r',
+					'Standing Arnold Press' : '3s x 12r',
+					'Dips' : '3s x 12-15r',
+					'Skull-Crusher' : '3s x 8-10r',
+					'Egyptian Lateral Raise' : '3s x 12r',
+					'Cable Tricep Kickback' : '3s x 20-30r'
+				},
+			 'Pull Day 1' :
+			 	{
+			 	 	'Pull-Ups' : '3s x 6r',
+			 	 	'Seated Cable Row' : '3s x 10-12r',
+			 	 	'Kneeling Cable Pullover' : '3s x 15-20r', 
+			 	 	'Hammer Cheat Curl' : '3s x 8-10r',
+			 	 	'Seated Incline Curls' : '2s x 12-15r',
+			 	 	'Hang Cleans' : '3s x 8-10r',
+			 	 	'Preacher Barbell Curls' : '3s x 10-12r'
+			 	},
+			 'Legs Day 1' : 
+			 	{
+			 		'Squat Pyramid Warmup' : {"""add as dict"""},
+			 		'Back Squat' : '4s x 4r',
+			 		'Romanian Deadlifts' : '3s x 10r',
+			 		'Bulgarian Split Squats' : '3s x 15r',
+			 		'Leg Extension Machine' : '3s x 10-12r',
+			 		'Seated Hamstring Curl' : '3s x 10-12r',
+			 		'Calf Raises' : '3s x 10-12r',
+			 		'Decline Crunches' : '3s x 10-12r',
+			 		'Tibialis Raises' : '3s x 10-12r',
+			 		'ATG Lunges' : '3s x 10-12r'
+			 	},
+			 'Push Day 2' : 
+			 	{
+			 		'Overhead Press' : '4s x 4r',
+			 		'Close Grip Flat Bench' : '3s x 8r',
+			 		'Close Grip Incline Bench' : '3s x 8r',
+			 		'Close Grip Decline Bench' : '3s x 8r',
+			 		'Cable Crossover' : '3s x 10-12r',
+			 		'Overhead Tricep Extension' : '3s x 10-12r',
+			 		'Dumbell Lateral Raises' : '3s x 7r/7r/7r',
+			 		'Neck Flexion' : '3s x 10-12r',
+			 		'Neck Extension' : '3s x 10-12r'
+			 	},
+			 'Pull Day 2' : 
+			 	{
+			 		'Lat Pulldown' : '3s x 10-12r',
+			 		'Chest-Supported Row' : '3s x 10-12r',
+			 		'Rope Face Pull' : '3s x 10-15r',
+			 		'Incline Dumbell Shrug' : '3s x 10-12r',
+			 		'Chest Fly Machine' : '2s x 10-15r',
+			 		'Reverse Barbell Curl' : '3s x 10r',
+			 		'Standard Barbell Curl' : '3s x 10r',
+			 		'Hang Clean' : '3s x 8r'
+			 	},
+ 			 'Legs Day 2' : 
+ 			 	{
+ 			 		'Deadlift Pyramid Warmup' : {"""add as dict"""},
+ 			 		'Deadlift' : '3s x 3r',
+ 			 		'Hack Squat' : '3s x 10-12r',
+ 			 		'Reverse Hack Squat' : '3s x 10-12r',
+ 			 		'Leg Press' : '3s x 10-12r',
+ 			 		'Single Leg Hip Thrust' : '2s x 15r',
+ 			 		'Hip Thrust' : '3s x 8r',
+ 			 		'Nordic Hamstring Curl' : '2s x 8-10r',
+ 			 		'Prisoner Back Extension' : '2s x 8-10r',
+ 			 		'Single Leg Calf Raise' : '3s x 8-10r',
+ 			 		'Weighted L-Sit Hold' : '3s x Failure',
+ 			 		'ATG Lunges' : '3s x 10-12r'
+ 			 	}
+			}
---
+++ code/tracker.py
@@ -0,0 +1,8 @@
+# This is the main Program that contains all of the main code.
+
+# 	Get the Date that the Workout took place On
+#	Take in what part of PPL you started on
+#	Take in Weights for each exercise
+import datetime
+import config as c
+
